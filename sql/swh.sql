9bd8c2c6e622b7f6fc8eb87c9d264382d310fb31
Stefano Zacchiroli <zack@upsilon.cc>
2015-09-03 16:15:38 +0200

    SQL: new modeling of file/directories
    (joint work with Nicolas and Antoine)

--- sql/swh.sql
+++ sql/swh.sql
@@ -22,7 +22,7 @@ create table dbversion
 );
 
 insert into dbversion(version, release, description)
-      values(6, now(), 'Work In Progress');
+      values(7, now(), 'Work In Progress');
 
 -- a Git object ID, i.e., a SHA1 checksum
 -- TODO rename this to (swh_?)object_id
@@ -36,6 +36,18 @@ create domain sha256 as text;
 
 -- a set of UNIX-like access permissions, as manipulated by, e.g., chmod
 create domain file_perms as int;
+
+-- Checksums about actual file content. Note that the content itself is not
+-- stored in the DB, but on external (key-value) storage. A single checksum is
+-- used as key there, but the other can be used to verify that we do not inject
+-- content collisions not knowingly.
+create table content
+(
+  id      git_object_id primary key,
+  sha1    sha1   not null,
+  sha256  sha256 not null,
+  length  bigint not null
+);
 
 -- An organization (or part thereof) that might be in charge of running
 -- software projects. Examples: Debian, GNU, GitHub, Apache, The Linux
@@ -132,7 +144,12 @@ create table project_history
 );
 
 -- A file-system directory.  A directory is a list of directory entries (see
--- table directory_entry).
+-- tables: directory_entry_{dir,file}).
+--
+-- To list the contents of a directory:
+-- 1. list the contained directory_entry_dir using table directory_list_dir
+-- 2. list the contained directory_entry_file using table directory_list_file
+-- 3. UNION
 --
 -- Synonyms/mappings:
 -- * git: tree
@@ -141,26 +158,44 @@ create table directory
   id  git_object_id primary key
 );
 
--- Directory entry types: file or directory.
-create type directory_entry_type as enum ('file', 'directory');
-
--- An individual entry contained in a directory.  A directory entry points to
--- either a file (= a leaf) or a directory (= an inner node). In both cases,
--- the pointer is a git_object_id. In the former case (files), the pointer can
--- be resolved using the content table; in the latter case (directories) using
--- the directory table.
-create table directory_entry
+-- A directory entry pointing to a sub-directory.
+create table directory_entry_dir
 (
-  name       text,  -- path name, relative to parent entry or root
-  id         git_object_id,  -- id of the object pointed to by this entry;
-                             -- might be a file or sub-dir, depending on type
-  type       directory_entry_type,  -- whether the entry is a file or a dir
-  perms      file_perms,   -- unix-like permissions
-  atime      timestamptz,  -- time of last access
-  mtime      timestamptz,  -- time of last modification
-  ctime      timestamptz,  -- time of last status change
-  directory  git_object_id references directory(id),  -- containing directory
-  primary key (name, directory)
+  id      bigserial primary key,
+  target  git_object_id references directory(id), -- id of target directory
+  name    text,  -- path name, relative to containing dir
+  perms   file_perms,   -- unix-like permissions
+  atime   timestamptz,  -- time of last access
+  mtime   timestamptz,  -- time of last modification
+  ctime   timestamptz   -- time of last status change
+);
+
+-- Mapping between directories and contained sub-directories.
+create table directory_list_dir
+(
+  dir_id     git_object_id references directory(id),
+  entry_id   bigint references directory_entry_dir(id),
+  primary key (dir_id, entry_id)
+);
+
+-- A directory entry pointing to a file.
+create table directory_entry_file
+(
+  id      bigserial primary key,
+  target  git_object_id references content(id), -- id of target file
+  name    text,  -- path name, relative to containing dir
+  perms   file_perms,   -- unix-like permissions
+  atime   timestamptz,  -- time of last access
+  mtime   timestamptz,  -- time of last modification
+  ctime   timestamptz   -- time of last status change
+);
+
+-- Mapping between directories and contained files.
+create table directory_list_file
+(
+  dir_id     git_object_id references directory(id),
+  entry_id   bigint references directory_entry_file(id),
+  primary key (dir_id, entry_id)
 );
 
 create table person
@@ -256,46 +291,6 @@ create table release
   comment   text,
   author     bigint references person(id)
 );
-
--- Checksums about actual file content. Note that the content itself is not
--- stored in the DB, but on external (key-value) storage. A single checksum is
--- used as key there, but the other can be used to verify that we do not inject
--- content collisions not knowingly.
-create table content
-(
-  id      git_object_id primary key,
-  sha1    sha1   not null,
-  sha256  sha256 not null,
-  length  bigint not null
-);
-
--- -- TODO work in progress
--- create table directory
--- (
---   id  git_object_id primary key
--- );
-
--- -- TODO work in progress
--- create table directory_path
--- (
---   directory         git_object_id not null,
---   directory_parent  git_object_id not null,
---   name              text,
---   atime             timestamptz,
---   ctime             timestamptz,
---   mtime             timestamptz,
---   perms             file_perms,
---   primary key(directory, directory_parent)
--- );
-
--- -- TODO work in progress
--- create table file
--- (
---   id         git_object_id references content(id),
---   directory  git_object_id references directory(id),
---   name       text,
---   primary key(id)
--- );
 
 
 commit;
