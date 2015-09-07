d8b04cb766a2e55d0d91247be849f046423d9013
Stefano Zacchiroli <zack@upsilon.cc>
2015-09-07 15:56:03 +0200

    db.Db: simple proxy for psycopg2 connection

--- swh/storage/db.py
+++ swh/storage/db.py
@@ -3,6 +3,27 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+import psycopg2
 
-class Storage:
-    pass
+
+class Db:
+    """DB proxy
+
+    """
+
+    def __init__(self, connstring):
+        """create a DB proxy, connecting to the DB
+
+        Args:
+            connstring: libpq connection string
+        """
+        self.conn = psycopg2.connect(connstring)
+
+    def cursor(self):
+        return self.conn.cursor()
+
+    def commit(self):
+        return self.conn.commit()
+
+    def rollback(self):
+        return self.conn.rollback()
