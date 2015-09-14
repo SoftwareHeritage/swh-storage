6c54578fb440b7f1a8b12bd1f244132d8eac7b92
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-14 10:38:41 +0200

    Add documentation for all the Storage.<>_add methods

--- swh/storage/storage.py
+++ swh/storage/storage.py
@@ -130,6 +130,20 @@ class Storage():
         """Add revisions to the storage
 
         Args:
+            revisions: iterable of dictionaries representing the individual
+                revisions to add. Each dict has the following keys:
+                - id (sha1_git): id of the revision to add
+                - date (datetime.DateTime): date the revision was written
+                - commit_date (datetime.DateTime): date the revision got added
+                    to the origin
+                - type (one of 'git', 'tar'): type of the revision added
+                - directory (sha1_git): the directory the revision points at
+                - message (bytes): the message associated with the revision
+                - author_name (bytes): the name of the revision author
+                - author_email (bytes): the email of the revision author
+                - committer_name (bytes): the name of the revision committer
+                - committer_email (bytes): the email of the revision committer
+                - parents (list of sha1_git): the parents of this revision
         """
         pass
 
@@ -145,6 +159,16 @@ class Storage():
         """Add releases to the storage
 
         Args:
+            releases: iterable of dictionaries representing the individual
+                releases to add. Each dict has the following keys:
+                - id (sha1_git): id of the release to add
+                - revision (sha1_git): id of the revision the release points
+                    to
+                - date (datetime.DateTime): the date the release was made
+                - name (bytes): the name of the release
+                - comment (bytes): the comment associated with the release
+                - author_name (bytes): the name of the release author
+                - author_email (bytes): the email of the release author
         """
         pass
 
@@ -160,6 +184,15 @@ class Storage():
         """Add occurrences to the storage
 
         Args:
+            occurrences: iterable of dictionaries representing the individual
+                occurrences to add. Each dict has the following keys:
+                - origin (int): id of the origin corresponding to the
+                    occurrence
+                - reference (bytes): the reference name of the occurrence
+                - revision (sha1_git): the id of the revision pointed to by
+                    the occurrence
+                - date (datetime.DateTime): the validity date for the given
+                    occurrence
         """
         pass
 
@@ -167,5 +200,9 @@ class Storage():
         """Add origins to the storage
 
         Args:
+            origins: iterable of dictionaries representing the individual
+                origins to add. Each dict has the following keys:
+                - type (FIXME: enum TBD): the origin type ('git', 'wget', ...)
+                - url (bytes): the url the origin points to
         """
         pass
