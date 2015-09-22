9185046fdc8c7e2302d3b39879bb4f560334aefa
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-22 16:02:03 +0200

    Add setup.py and friends

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+def parse_requirements():
+    requirements = []
+    with open('requirements.txt') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
+
+    return requirements
+
+
+setup(
+    name='swh.storage',
+    description='Software Heritage storage manager',
+    author='Software Heritage developers',
+    author_email='swh-devel@inria.fr',
+    url='https://forge.softwareheritage.org/diffusion/DSTO/',
+    packages=['swh.storage', 'swh.storage.tests'],
+    scripts=[
+        'bin/swh-objstorage-add-dir',
+        'bin/swh-objstorage-fsck',
+        'bin/swh-storage-add-dir',
+    ],
+    install_requires=parse_requirements(),
+    setup_requires=['vcversioner'],
+    vcversioner={},
+    include_package_data=True,
+)
