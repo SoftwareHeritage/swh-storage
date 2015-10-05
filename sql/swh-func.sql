b3aac6a9c2dd754d3d508fc6eaa4775a0a369e50
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-10-05 18:38:05 +0200

    swh-func: add swh_occurrence_history_add()

--- sql/swh-func.sql
+++ sql/swh-func.sql
@@ -510,6 +510,44 @@ begin
 end
 $$;
 
+-- add tmp_occurrence_history entries to occurrence_history
+--
+-- operates in bulk: 0. swh_mktemp(occurrence_history), 1. COPY to tmp_occurrence_history,
+-- 2. call this function
+create or replace function swh_occurrence_history_add()
+    returns void
+    language plpgsql
+as $$
+begin
+    -- Update intervals we have the data to update
+    with new_intervals as (
+        select t.origin, t.branch, t.authority, t.validity,
+	       o.validity - t.validity as new_validity
+	from tmp_occurrence_history t
+        left join occurrence_history o
+        using (origin, branch, authority)
+	where o.origin is not null),
+    -- do not update intervals if they would become empty (perfect overlap)
+    to_update as (
+        select * from new_intervals
+	where not isempty(new_validity))
+    update occurrence_history o set validity = t.new_validity
+    from to_update t
+    where o.origin = t.origin and o.branch = t.branch and o.authority = t.authority;
+
+    -- Now only insert intervals that aren't already present
+    insert into occurrence_history (origin, branch, revision, authority, validity)
+	select distinct origin, branch, revision, authority, validity
+	from tmp_occurrence_history t
+	where not exists (
+	    select 1 from occurrence_history o
+	    where o.origin = t.origin and o.branch = t.branch and
+	          o.authority = t.authority and o.revision = t.revision and
+		  o.validity = t.validity);
+    return;
+end
+$$;
+
 
 -- Absolute path: directory reference + complete path relative to it
 create type content_dir as (
