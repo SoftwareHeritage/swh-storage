b5780ee2d1b33ce33e6d9a02bec6fc3e9fb5a3f9
Nicolas Dandrimont <nicolas@dandrimont.eu>
2016-08-11 19:33:09 +0200

    swh-func: add functions to retrieve objects by object_id
    References: T525

--- sql/swh-func.sql
+++ sql/swh-func.sql
@@ -1262,6 +1262,65 @@ as $$
   from entity_hierarchy;
 $$;
 
+
+-- Object listing by object_id
+
+create or replace function swh_content_list_by_object_id(
+    min_excl bigint,
+    max_incl bigint
+)
+    returns setof content
+    language sql
+    stable
+as $$
+    select * from content
+    where object_id > min_excl and object_id <= max_incl
+    order by object_id;
+$$;
+
+create or replace function swh_revision_list_by_object_id(
+    min_excl bigint,
+    max_incl bigint
+)
+    returns setof revision_entry
+    language sql
+    stable
+as $$
+    with revs as (
+        select * from revision
+        where object_id > min_excl and object_id <= max_incl
+    )
+    select r.id, r.date, r.date_offset, r.date_neg_utc_offset,
+           r.committer_date, r.committer_date_offset, r.committer_date_neg_utc_offset,
+           r.type, r.directory, r.message,
+           a.id, a.fullname, a.name, a.email, c.id, c.fullname, c.name, c.email, r.metadata, r.synthetic,
+           array(select rh.parent_id::bytea from revision_history rh where rh.id = r.id order by rh.parent_rank)
+               as parents, r.object_id
+    from revs r
+    left join person a on a.id = r.author
+    left join person c on c.id = r.committer
+    order by r.object_id;
+$$;
+
+create or replace function swh_release_list_by_object_id(
+    min_excl bigint,
+    max_incl bigint
+)
+    returns setof release_entry
+    language sql
+    stable
+as $$
+    with rels as (
+        select * from release
+        where object_id > min_excl and object_id <= max_incl
+    )
+    select r.id, r.target, r.target_type, r.date, r.date_offset, r.date_neg_utc_offset, r.name, r.comment,
+           r.synthetic, p.id as author_id, p.fullname as author_fullname, p.name as author_name, p.email as author_email, r.object_id
+    from rels r
+    left join person p on p.id = r.author
+    order by r.object_id;
+$$;
+
 -- simple counter mapping a textual label to an integer value
 create type counter as (
     label  text,
