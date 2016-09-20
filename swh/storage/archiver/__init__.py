2b18a7acc70ff6482ac360b737000c05d1602731
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-09-20 12:35:10 +0200

    Remove optional dependency on swh.objstorage.cloud
    If you want the archiver to have cloud abilities, install the
    package python3-swh.objstorage.cloud on the server as well.

--- swh/storage/archiver/__init__.py
+++ swh/storage/archiver/__init__.py
@@ -3,11 +3,3 @@ from .director import ArchiverStdinToBackendDirector       # NOQA
 from .worker import ArchiverWithRetentionPolicyWorker      # NOQA
 from .worker import ArchiverToBackendWorker                # NOQA
 from .copier import ArchiverCopier                         # NOQA
-
-from swh.objstorage import register_objstorages
-from swh.objstorage.cloud import AzureCloudObjStorage  # noqa
-
-# Register new objstorage abilities
-register_objstorages({
-    'azure-storage': AzureCloudObjStorage
-})
