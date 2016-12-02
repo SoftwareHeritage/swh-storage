43b37dc69c45ab12b40328085805039027f746a8
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-12-02 10:20:02 +0100

    Update indexer configuration - Index/constraints - pass 2
    Related T610

--- sql/upgrades/097.sql
+++ sql/upgrades/097.sql
@@ -6,6 +6,10 @@
 insert into dbversion(version, release, description)
       values(97, now(), 'Work In Progress');
 
+------------------------
+-- Update Schema + data
+------------------------
+
 update indexer_configuration
 set tool_configuration='{"command_line": "nomossa <filepath>"}'
 where tool_name='nomos' and tool_version='3.1.0rc2-31-ga2cbb8c';
@@ -19,29 +23,83 @@ values ('pygments', '2.0.1+dfsg-1.1+deb8u1', '{"type": "library", "debian-packag
 insert into indexer_configuration(tool_name, tool_version, tool_configuration)
 values ('file', '5.22', '{"command_line": "file --mime <filepath>"}');
 
-alter table content_ctags add column indexer_configuration_id bigserial;
-
-update content_ctags
-set indexer_configuration_id = (select id from indexer_configuration where tool_name='universal-ctags');
+-- ctags
 
 alter table content_ctags
-add constraint content_ctags_indexer_configuration_id_idx
-foreign key (indexer_configuration_id) references indexer_configuration(id);
+  add column indexer_configuration_id bigserial;
 
-alter table content_language add column indexer_configuration_id bigserial;
+comment on column content_ctags.indexer_configuration_id is 'Tool used to compute the information';
+
+update content_ctags
+set indexer_configuration_id = (select id
+                                from indexer_configuration
+                                where tool_name='universal-ctags');
+
+alter table content_ctags
+  alter column indexer_configuration_id set not null;
+
+alter table content_ctags
+  add constraint content_ctags_indexer_configuration_id_idx
+  foreign key (indexer_configuration_id) references indexer_configuration(id);
+
+drop index content_ctags_id_md5_kind_line_lang_idx;
+create unique index on content_ctags(id, md5(name), kind, line, lang, indexer_configuration_id);
+
+-- language
+
+alter table content_language
+  add column indexer_configuration_id bigserial;
+
+comment on column content_language.indexer_configuration_id is 'Tool used to compute the information';
 
 update content_language
 set indexer_configuration_id = (select id from indexer_configuration where tool_name='pygments');
 
 alter table content_language
-add constraint content_language_indexer_configuration_id_idx
-foreign key (indexer_configuration_id) references indexer_configuration(id);
+  alter column indexer_configuration_id set not null;
 
-alter table content_mimetype add column indexer_configuration_id bigserial;
+alter table content_language
+  add constraint content_language_indexer_configuration_id_idx
+  foreign key (indexer_configuration_id) references indexer_configuration(id);
+
+alter table content_language
+  drop constraint content_language_pkey;
+
+alter table content_language
+  add primary key(id, indexer_configuration_id);
+
+-- mimetype
+
+alter table content_mimetype
+  add column indexer_configuration_id bigserial;
+
+comment on column content_mimetype.indexer_configuration_id is 'Tool used to compute the information';
 
 update content_mimetype
 set indexer_configuration_id = (select id from indexer_configuration where tool_name='file');
 
 alter table content_mimetype
-add constraint content_mimetype_indexer_configuration_id_idx
-foreign key (indexer_configuration_id) references indexer_configuration(id);
+  alter column indexer_configuration_id
+  set not null;
+
+alter table content_mimetype
+  add constraint content_mimetype_indexer_configuration_id_idx
+  foreign key (indexer_configuration_id) references indexer_configuration(id);
+
+alter table content_mimetype
+  drop constraint content_mimetype_pkey;
+
+alter table content_mimetype
+  add primary key(id, indexer_configuration_id);
+
+-- fossology-license
+
+comment on column content_fossology_license.indexer_configuration_id is 'Tool used to compute the information';
+
+alter table content_fossology_license
+  alter column indexer_configuration_id
+  set not null;
+
+alter table content_fossology_license
+  add primary key using index content_fossology_license_id_license_id_indexer_configurati_idx;
+
