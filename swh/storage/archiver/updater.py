1dc2069d3ab0b9c1780302fdf3ab102dac0aeef5
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-02-27 12:12:30 +0000

    swh.storage.archiver.updater: Call directly content_archive_add
    This function will drop any existing contents if encountered
    
    Related T494
    Related T569

--- swh/storage/archiver/updater.py
+++ swh/storage/archiver/updater.py
@@ -18,9 +18,7 @@ class SWHArchiverContentUpdater(SWHJournalClient):
     ADDITIONAL_CONFIG = {
         'archiver_storage_conn': (
             'str', 'dbname=softwareheritage-archiver-dev user=guest'),
-        'sources_missing': ('list[str]', ['banco', 'azure']),
         'sources_present': ('list[str]', ['uffizi'])
-
     }
 
     def __init__(self):
@@ -28,19 +26,14 @@ class SWHArchiverContentUpdater(SWHJournalClient):
         super().__init__(extra_configuration={'object_types': ['content']})
 
         self.sources_present = self.config['sources_present']
-        self.sources_missing = self.config['sources_missing']
 
         self.archiver_storage = ArchiverStorage(
             self.config['archiver_storage_conn'])
 
     def process_objects(self, messages):
-        key_id = b'sha1'
-
-        content_ids = [{'content_id': c[key_id]} for c in messages['content']]
-        unknowns = self.archiver_storage.content_archive_get_unknown(
-            content_ids)
-        self.archiver_storage.content_archive_content_add(
-            unknowns, self.sources_present, self.sources_missing)
+        self.archiver_storage.content_archive_add(
+            (c[b'sha1'] for c in messages['content']),
+            self.sources_present)
 
 
 if __name__ == '__main__':
