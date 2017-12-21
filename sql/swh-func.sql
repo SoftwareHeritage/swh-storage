11fcb82a4f8fa1cc06582cd7000cdedbdb149c63
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-12-21 19:38:50 +0100

    Refactor swh_snapshot_add to hit more indexes

--- sql/swh-func.sql
+++ sql/swh-func.sql
@@ -958,23 +958,29 @@ begin
   select object_id from snapshot where id = snapshot_id into snapshot_object_id;
   if snapshot_object_id is null then
      insert into snapshot (id) values (snapshot_id) returning object_id into snapshot_object_id;
-     with all_branches(name, target_type, target) as (
-       select name, target_type, target from tmp_snapshot_branch
-     ), inserted as (
-       insert into snapshot_branch (name, target_type, target)
-       select name, target_type, target from all_branches
-       on conflict do nothing
-       returning object_id
-     )
+     insert into snapshot_branch (name, target_type, target)
+       select name, target_type, target from tmp_snapshot_branch tmp
+       where not exists (
+         select 1
+         from snapshot_branch sb
+         where sb.name = tmp.name
+           and sb.target = tmp.target
+           and sb.target_type = tmp.target_type
+       )
+       on conflict do nothing;
      insert into snapshot_branches (snapshot_id, branch_id)
-     select snapshot_object_id, object_id as branch_id from inserted
-     union all
-     select snapshot_object_id, object_id as branch_id
-       from all_branches ab
+     select snapshot_object_id, sb.object_id as branch_id
+       from tmp_snapshot_branch tmp
        join snapshot_branch sb
-         on sb.name = ab.name
-           and sb.target_type is not distinct from ab.target_type
-           and sb.target is not distinct from ab.target;
+       using (name, target, target_type)
+       where tmp.target is not null and tmp.target_type is not null
+     union
+     select snapshot_object_id, sb.object_id as branch_id
+       from tmp_snapshot_branch tmp
+       join snapshot_branch sb
+       using (name)
+       where tmp.target is null and tmp.target_type is null
+         and sb.target is null and sb.target_type is null;
   end if;
   update origin_visit ov
     set snapshot_id = snapshot_object_id
