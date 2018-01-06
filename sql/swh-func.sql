cf6cac74260c7f4f756ccabcaaa16e2e56c84e2c
Stefano Zacchiroli <zack@upsilon.cc>
2018-01-06 10:21:51 +0100

    SQL: move definition of hash_sha1() function to swh-func.sql
    rationale: swh-init.sql should only contain stuff that requires being Postgres
    superuser

--- sql/swh-func.sql
+++ sql/swh-func.sql
@@ -1,3 +1,12 @@
+
+create or replace function hash_sha1(text)
+       returns text
+as $$
+   select encode(digest($1, 'sha1'), 'hex')
+$$ language sql strict immutable;
+
+comment on function hash_sha1(text) is 'Compute SHA1 hash as text';
+
 -- create a temporary table called tmp_TBLNAME, mimicking existing table
 -- TBLNAME
 --
