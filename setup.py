63c924d4acb09c9d8a185619bfc4e17af9692efb
David Douard <david.douard@sdfa3.org>
2018-10-04 17:15:31 +0200

    setup: prepare for pypi upload
    related to T1242

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+# Copyright (C) 2015-2018  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
 
-import os
 from setuptools import setup, find_packages
+
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 def parse_requirements(name=None):
@@ -11,7 +23,7 @@ def parse_requirements(name=None):
         reqf = 'requirements.txt'
 
     requirements = []
-    if not os.path.exists(reqf):
+    if not path.exists(reqf):
         return requirements
 
     with open(reqf) as f:
@@ -26,6 +38,8 @@ def parse_requirements(name=None):
 setup(
     name='swh.storage',
     description='Software Heritage storage manager',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Software Heritage developers',
     author_email='swh-devel@inria.fr',
     url='https://forge.softwareheritage.org/diffusion/DSTO/',
@@ -33,13 +47,25 @@ setup(
     scripts=[
         'bin/swh-storage-add-dir',
     ],
+    setup_requires=['vcversioner'],
     install_requires=parse_requirements() + parse_requirements('swh'),
     extras_require={
         'testing': parse_requirements('test'),
         'schemata': ['SQLAlchemy'],
         'listener': ['kafka_python'],
     },
-    setup_requires=['vcversioner'],
     vcversioner={},
     include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    project_urls={
+        'Bug Reports': 'https://forge.softwareheritage.org/maniphest',
+        'Funding': 'https://www.softwareheritage.org/donate',
+        'Source': 'https://forge.softwareheritage.org/source/swh-storage',
+    },
 )
