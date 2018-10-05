fb794bf4c0f92bc3219dca5e6906a2c3e9e85ae6
Antoine Pietri <antoine.pietri1@gmail.com>
2018-10-05 12:56:14 +0200

    Add requirements-test.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup, find_packages
 
 
-def parse_requirements():
+def parse_requirements(name=None):
+    if name:
+        reqf = 'requirements-%s.txt' % name
+    else:
+        reqf = 'requirements.txt'
+
     requirements = []
-    for reqf in ('requirements.txt', 'requirements-swh.txt'):
-        with open(reqf) as f:
-            for line in f.readlines():
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-                requirements.append(line)
+    if not os.path.exists(reqf):
+        return requirements
+
+    with open(reqf) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
     return requirements
 
 
@@ -25,8 +33,9 @@ setup(
     scripts=[
         'bin/swh-storage-add-dir',
     ],
-    install_requires=parse_requirements(),
+    install_requires=parse_requirements() + parse_requirements('swh'),
     extras_require={
+        'testing': parse_requirements('test'),
         'schemata': ['SQLAlchemy'],
         'listener': ['kafka_python'],
     },
