c3803ef8f79765c5ff4f0e13e115779aea7ad38f
David Douard <david.douard@sdfa3.org>
2020-07-09 11:54:07 +0200

    Fix a typo I introduced in previous revision
    dict(x if x is not None else None) != dict(x) if x is not None else None...

--- swh/storage/cassandra/converters.py
+++ swh/storage/cassandra/converters.py
@@ -36,7 +36,7 @@ def revision_to_db(revision: Revision) -> Dict[str, Any]:
     if not extra_headers and metadata and "extra_headers" in metadata:
         extra_headers = db_revision["metadata"].pop("extra_headers")
     db_revision["metadata"] = json.dumps(
-        dict(db_revision["metadata"] if db_revision["metadata"] is not None else None)
+        dict(db_revision["metadata"]) if db_revision["metadata"] is not None else None
     )
     db_revision["extra_headers"] = extra_headers
     db_revision["type"] = db_revision["type"].value
