6a53cb3ef3ad769ab015fa011e0a14702d1a14cf
Valentin Lorentz <vlorentz@softwareheritage.org>
2020-08-19 14:25:22 +0200

    pg: Check revision.extra_headers is not null.
    Fixes a regression in 038a219f84d6b8a4f02b48f9ad3c5d823d097790,
    as it made the converter expect a list.
    
    When adding this column, we made it default to null instead of defaulting
    to an empty array, so existing records were initialized will null.
    
    This commit migrates these nulls to empty arrays, then adds a constraint
    to enforce it in the future.

---
+++ sql/upgrades/161.sql
@@ -0,0 +1,17 @@
+-- SWH DB schema upgrade
+-- from_version: 160
+-- to_version: 161
+-- description: Make revision.extra_headers not null
+
+-- latest schema version
+insert into dbversion(version, release, description)
+      values(161, now(), 'Work Still In Progress');
+
+
+update revision
+    set extra_headers = ARRAY[][]
+    where extra_headers is null;
+
+
+alter table revision
+    alter column extra_headers set not null;
