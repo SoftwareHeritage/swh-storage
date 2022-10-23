SoftwareHeritage/swh-storage

commit 0e8da810ac962b79dda18d9cfe9f3c7ea4f9de52

tests: only flush() the kafka journal writer once per test
instead of flushing it n times per test. Since the call to kafka
Producer.flush() takes about 1s, reducing the number of calls to this
method significantly reduce the execution time of the tests.

This required a small refactoring of the JournalBackfiller class to make
the journal writer live out of the scope of the run() so the test can
access the journal writer instance and call the flush() method.

Requires swh-journal >= 1.2.0

swh/storage/interface.py
@@ -5,7 +5,17 @@
 
 import datetime
 from enum import Enum
-from typing import Any, Dict, Iterable, List, Optional, Sequence, Tuple, TypeVar
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    TypeVar,
+)
 
 import attr
 from typing_extensions import Protocol, TypedDict, runtime_checkable
@@ -33,6 +43,9 @@ from swh.model.model import (
     SnapshotBranch,
 )
 from swh.model.swhids import ExtendedSWHID, ObjectType
+
+if TYPE_CHECKING:
+    from swh.storage.writer import JournalWriter
 
 
 class ListOrder(Enum):
@@ -76,6 +89,8 @@ def deprecated(f):
 
 @runtime_checkable
 class StorageInterface(Protocol):
+    journal_writer: Optional["JournalWriter"]
+
     @remote_api_endpoint("check_config")
     def check_config(self, *, check_write: bool) -> bool:
         """Check that the storage is configured and ready to go."""

swh/storage/tests/test_backfill.py
@@ -235,6 +235,7 @@ def test_backfiller(
         "brokers": [kafka_server],
         "client_id": "kafka_writer-1",
         "prefix": prefix1,
+        "auto_flush": False,
     }
     swh_storage_backend_config["journal_writer"] = journal1
     storage = get_storage(**swh_storage_backend_config)
@@ -242,6 +243,8 @@ def test_backfiller(
     for object_type, objects in TEST_OBJECTS.items():
         method = getattr(storage, object_type + "_add")
         method(objects)
+    assert storage.journal_writer is not None
+    storage.journal_writer.journal.flush()
 
     # now apply the backfiller on the storage to fill the journal under prefix2
     backfiller_config = {
@@ -249,6 +252,7 @@ def test_backfiller(
             "brokers": [kafka_server],
             "client_id": "kafka_writer-2",
             "prefix": prefix2,
+            "auto_flush": False,
         },
         "storage": swh_storage_backend_config,
     }
@@ -257,6 +261,7 @@ def test_backfiller(
     backfiller = JournalBackfiller(backfiller_config)
     for object_type in TEST_OBJECTS:
         backfiller.run(object_type, None, None)
+    backfiller.writer.journal.flush()
 
     # Trace log messages for unhandled object types in the replayer
     caplog.set_level(logging.DEBUG, "swh.storage.replay")

swh/storage/tests/test_kafka_writer.py
@@ -25,6 +25,7 @@ def test_storage_direct_writer(kafka_prefix: str, kafka_server, consumer: Consum
         "client_id": "kafka_writer",
         "prefix": kafka_prefix,
         "anonymize": False,
+        "auto_flush": False,
     }
     storage_config: Dict[str, Any] = {
         "cls": "pipeline",
@@ -58,6 +59,8 @@ def test_storage_direct_writer(kafka_prefix: str, kafka_server, consumer: Consum
             expected_messages += len(objs)
         else:
             assert False, obj_type
+    assert storage.journal_writer is not None
+    storage.journal_writer.journal.flush()
 
     existing_topics = set(
         topic
@@ -97,6 +100,7 @@ def test_storage_direct_writer_anonymized(
         "client_id": "kafka_writer",
         "prefix": kafka_prefix,
         "anonymize": True,
+        "auto_flush": False,
     }
     storage_config: Dict[str, Any] = {
         "cls": "pipeline",
@@ -117,6 +121,8 @@ def test_storage_direct_writer_anonymized(
         method = getattr(storage, obj_type + "_add")
         method(objs)
         expected_messages += len(objs)
+    assert storage.journal_writer is not None
+    storage.journal_writer.journal.flush()
 
     existing_topics = set(
         topic

swh/storage/tests/test_replay.py
@@ -70,6 +70,7 @@ def replayer_storage_and_client(
         "brokers": [kafka_server],
         "client_id": "kafka_writer",
         "prefix": kafka_prefix,
+        "auto_flush": False,
     }
     storage_config: Dict[str, Any] = {
         "cls": "memory",
@@ -106,6 +107,7 @@ def test_storage_replayer(replayer_storage_and_client, caplog):
         method = getattr(src, object_type + "_add")
         method(objects)
         nb_sent += len(objects)
+    src.journal_writer.journal.flush()
 
     caplog.set_level(logging.ERROR, "swh.journal.replay")
 
@@ -145,6 +147,7 @@ def test_storage_replay_with_collision(replayer_storage_and_client, caplog):
         method = getattr(src, object_type + "_add")
         method(objects)
         nb_sent += len(objects)
+    src.journal_writer.journal.flush()
 
     # Create collision in input data
     # These should not be written in the destination
@@ -341,6 +344,7 @@ def test_storage_replay_anonymized(
         "client_id": "kafka_writer",
         "prefix": kafka_prefix,
         "anonymize": True,
+        "auto_flush": False,
     }
     src_config: Dict[str, Any] = {"cls": "memory", "journal_writer": writer_config}
 
@@ -355,6 +359,8 @@ def test_storage_replay_anonymized(
         method = getattr(storage, obj_type + "_add")
         method(objs)
         nb_sent += len(objs)
+    assert storage.journal_writer is not None
+    storage.journal_writer.journal.flush()
 
     # Fill a destination storage from Kafka, potentially using privileged topics
     dst_storage = get_storage(cls="memory")
@@ -404,6 +410,7 @@ def test_storage_replayer_with_validation_ok(
         method = getattr(src, object_type + "_add")
         method(objects)
         nb_sent += len(objects)
+    src.journal_writer.journal.flush()
 
     # Fill the destination storage from Kafka
     dst = get_storage(cls="memory")
@@ -466,6 +473,8 @@ def test_storage_replayer_with_validation_nok(
     topic = f"{src.journal_writer.journal._prefix}.directory"
     src.journal_writer.journal.send(topic, dict_repr["id"], dict_repr)
     nb_sent += 1
+
+    src.journal_writer.journal.flush()
 
     # Fill the destination storage from Kafka
     dst = get_storage(cls="memory")

swh/storage/backfill.py
@@ -598,6 +598,15 @@ class JournalBackfiller:
     def __init__(self, config=None):
         self.config = config
         self.check_config(config)
+        self._db = None
+        self.writer = JournalWriter({"cls": "kafka", **self.config["journal_writer"]})
+        assert self.writer.journal is not None
+
+    @property
+    def db(self):
+        if self._db is None:
+            self._db = BaseDb.connect(self.config["storage"]["db"])
+        return self._db
 
     def check_config(self, config):
         missing_keys = []
@@ -653,10 +662,6 @@ class JournalBackfiller:
             object_type, start_object, end_object
         )
 
-        db = BaseDb.connect(self.config["storage"]["db"])
-        writer = JournalWriter({"cls": "kafka", **self.config["journal_writer"]})
-        assert writer.journal is not None
-
         for range_start, range_end in RANGE_GENERATORS[object_type](
             start_object, end_object
         ):
@@ -667,10 +672,10 @@ class JournalBackfiller:
                 _format_range_bound(range_end),
             )
 
-            objects = fetch(db, object_type, start=range_start, end=range_end)
+            objects = fetch(self.db, object_type, start=range_start, end=range_end)
 
             if not dry_run:
-                writer.write_additions(object_type, objects)
+                self.writer.write_additions(object_type, objects)
             else:
                 # only consume the objects iterator to check for any potential
                 # decoding/encoding errors

commit 82ad28bb29f52424b89a12fbaba286fce1c13c1e

Fix documentation build
It was broken by 0e8da810ac962b79dda18d9cfe9f3c7ea4f9de52
because the conditional import prevents sphinx from detecting
where the class is imported from.

Additionally, this attribute should not be on the interface,
because proxies do not have it, but they are expected to have
that interface.

swh/storage/interface.py
@@ -5,17 +5,7 @@
 
 import datetime
 from enum import Enum
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    Dict,
-    Iterable,
-    List,
-    Optional,
-    Sequence,
-    Tuple,
-    TypeVar,
-)
+from typing import Any, Dict, Iterable, List, Optional, Sequence, Tuple, TypeVar
 
 import attr
 from typing_extensions import Protocol, TypedDict, runtime_checkable
@@ -43,9 +33,6 @@ from swh.model.model import (
     SnapshotBranch,
 )
 from swh.model.swhids import ExtendedSWHID, ObjectType
-
-if TYPE_CHECKING:
-    from swh.storage.writer import JournalWriter
 
 
 class ListOrder(Enum):
@@ -89,8 +76,6 @@ def deprecated(f):
 
 @runtime_checkable
 class StorageInterface(Protocol):
-    journal_writer: Optional["JournalWriter"]
-
     @remote_api_endpoint("check_config")
     def check_config(self, *, check_write: bool) -> bool:
         """Check that the storage is configured and ready to go."""

swh/storage/tests/test_backfill.py
@@ -243,8 +243,7 @@ def test_backfiller(
     for object_type, objects in TEST_OBJECTS.items():
         method = getattr(storage, object_type + "_add")
         method(objects)
-    assert storage.journal_writer is not None
-    storage.journal_writer.journal.flush()
+    storage.journal_writer.journal.flush()  # type: ignore[attr-defined]
 
     # now apply the backfiller on the storage to fill the journal under prefix2
     backfiller_config = {

swh/storage/tests/test_kafka_writer.py
@@ -59,8 +59,7 @@ def test_storage_direct_writer(kafka_prefix: str, kafka_server, consumer: Consum
             expected_messages += len(objs)
         else:
             assert False, obj_type
-    assert storage.journal_writer is not None
-    storage.journal_writer.journal.flush()
+    storage.journal_writer.journal.flush()  # type: ignore[attr-defined]
 
     existing_topics = set(
         topic
@@ -121,8 +120,7 @@ def test_storage_direct_writer_anonymized(
         method = getattr(storage, obj_type + "_add")
         method(objs)
         expected_messages += len(objs)
-    assert storage.journal_writer is not None
-    storage.journal_writer.journal.flush()
+    storage.journal_writer.journal.flush()  # type: ignore[attr-defined]
 
     existing_topics = set(
         topic

swh/storage/tests/test_replay.py
@@ -359,8 +359,7 @@ def test_storage_replay_anonymized(
         method = getattr(storage, obj_type + "_add")
         method(objs)
         nb_sent += len(objs)
-    assert storage.journal_writer is not None
-    storage.journal_writer.journal.flush()
+    storage.journal_writer.journal.flush()  # type: ignore[attr-defined]
 
     # Fill a destination storage from Kafka, potentially using privileged topics
     dst_storage = get_storage(cls="memory")