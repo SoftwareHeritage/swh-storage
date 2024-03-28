57556e71e11d80dad0b8550417501100d98d0462
Nicolas Dandrimont <nicolas@dandrimont.eu>
2024-03-28 16:09:11 +0100

    metrics: Introduce a DifferentialTimer statsd helper
    This helper computes the timing difference between an inner function and
    an outer block, and sends the result to statsd with a set of tags. This
    will allow us to measure the overhead of some endpoints.

--- swh/storage/metrics.py
+++ swh/storage/metrics.py
@@ -3,10 +3,16 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+from contextlib import contextmanager
 from functools import wraps
 import logging
+from time import monotonic
+from types import TracebackType
+from typing import Dict, Optional, Type
 
 from swh.core.statsd import statsd
+
+logger = logging.getLogger(__name__)
 
 OPERATIONS_METRIC = "swh_storage_operations_total"
 OPERATIONS_UNIT_METRIC = "swh_storage_operations_{unit}_total"
@@ -22,6 +28,89 @@ def timed(f):
             return f(*a, **kw)
 
     return d
+
+
+class DifferentialTimer:
+    """Compute differential timing metrics and send them to StatsD.
+
+    The DifferentialTimer will send the ``metric`` to statsd, computing the
+    difference between the time spent within the whole block, with the time
+    spent within any blocks wrapped in the :meth:`inner` decorator.
+
+    If an exception is raised,when sending the metric, the DifferentialTimer
+    will add a ``inner_exc`` (when the exception is raised by an inner-timed
+    section) or ``outer_exc`` tag with the name of the exception class that was
+    raised .
+
+    Arguments:
+      metric: name of the timing metric that is sent to StatsD when the context
+        manager exits
+      tags: tags to attach to the metric when it is sent
+
+    Example:
+      To generate a ``metric_seconds`` timed metric, recording the overhead of
+      running ``run_pre_processing`` and ``run_post_processing``, use:
+
+      .. code-block:: python
+
+          with DifferentialTimer("metric_seconds") as t:
+              run_pre_processing()
+              with t.inner():
+                  run_inner_method()
+              run_post_processing()
+
+    """
+
+    def __init__(self, metric: str, tags: Optional[Dict[str, str]] = None) -> None:
+        self.start_ts: Optional[float] = None
+        self.inner_elapsed = 0
+        self.exc_source = "outer"
+        self.metric = metric
+        self.tags = tags or {}
+
+    def __enter__(self) -> "DifferentialTimer":
+        self.start_ts = monotonic()
+        return self
+
+    @contextmanager
+    def inner(self):
+        """Add the duration of this block to the inner elapsed time counter."""
+        start = monotonic()
+        try:
+            yield
+        except BaseException:
+            self.exc_source = "inner"
+            raise
+        finally:
+            self.inner_elapsed += monotonic() - start
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        _exc_value: Optional[BaseException],
+        _traceback: Optional[TracebackType],
+    ) -> None:
+        if self.start_ts is None:
+            logger.warning("DifferentialTimer exited without being started")
+            return
+
+        total_elapsed = monotonic() - self.start_ts
+
+        extra_tags = {}
+        if exc_type:
+            extra_tags[f"{self.exc_source}_exc"] = exc_type.__name__
+
+        if self.inner_elapsed == 0 and not exc_type:
+            logger.warning("DifferentialTimer didn't record calls to `inner`?")
+
+        # Statsd expects timings in milliseconds
+        value = (total_elapsed - self.inner_elapsed) * 1000
+
+        statsd.timing(
+            self.metric,
+            value,
+            tags={**self.tags, **extra_tags},
+        )
 
 
 def send_metric(metric, count, method_name):
