576de21579a0d1046c0a04a3f6b535b7d8205b42
Jérémy Bobbio (Lunar) <lunar@softwareheritage.org>
2024-04-11 13:42:33 +0000

    Add a test for str() on MaskedObjectException

--- swh/storage/tests/test_exception.py
+++ swh/storage/tests/test_exception.py
@@ -30,3 +30,37 @@ def test_hash_collision_exception():
     assert exc.colliding_contents == [content_hex_hashes(content)]
 
     assert exc.colliding_content_hashes() == [content]
+
+
+def test_masked_object_exception_str() -> None:
+    from uuid import UUID
+
+    from swh.model.swhids import ExtendedSWHID
+
+    from ..exc import MaskedObjectException
+    from ..proxies.masking.db import MaskedState, MaskedStatus
+
+    swhid_cnt = ExtendedSWHID.from_string(
+        "swh:1:cnt:a6d1888d25fd8e84986f386ee0cdc8a1fc3c09b4"
+    )
+    swhid_ori = ExtendedSWHID.from_string(
+        "swh:1:ori:155291d5b9ada4570672510509f93fcfd9809882"
+    )
+    status1 = MaskedStatus(
+        state=MaskedState.DECISION_PENDING,
+        request=UUID("da785a27-7e59-4a35-b82a-a5ae3714407c"),
+    )
+    status2 = MaskedStatus(
+        state=MaskedState.RESTRICTED,
+        request=UUID("4fd42e35-2b6c-4536-8447-bc213cd0118b"),
+    )
+    masked = {swhid_cnt: [status1], swhid_ori: [status1, status2]}
+    exc = MaskedObjectException(masked)
+
+    assert str(exc) == (
+        "Some objects are masked: "
+        "swh:1:cnt:a6d1888d25fd8e84986f386ee0cdc8a1fc3c09b4 by request "
+        "da785a27-7e59-4a35-b82a-a5ae3714407c (DECISION_PENDING), "
+        "swh:1:ori:155291d5b9ada4570672510509f93fcfd9809882 by request "
+        "da785a27-7e59-4a35-b82a-a5ae3714407c (DECISION_PENDING) and others"
+    )
