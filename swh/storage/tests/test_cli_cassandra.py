2a670431983623cf65194b42b087072786c431bd
Antoine Lambert <anlambert@softwareheritage.org>
2025-09-18 22:01:57 +0200

    test_cli_cassandra: Remove redundant code with fixture

--- swh/storage/tests/test_cli_cassandra.py
+++ swh/storage/tests/test_cli_cassandra.py
@@ -1,11 +1,10 @@
-# Copyright (C) 2020  The Software Heritage developers
+# Copyright (C) 2020-2025  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
 import functools
 import logging
-import os
 
 from cassandra import InvalidRequest
 import pytest
@@ -42,32 +41,22 @@ def invoke(swh_storage_cassandra_backend_config):
 class TestCassandraCli:
     def test_init(
         self,
-        swh_storage_cassandra_cluster,
-        cassandra_auth_provider_config,
+        swh_storage_cassandra_backend_config,
     ):
         # not using the the invoke fixture because it uses the keyspace that is already
         # initialized, which would make this test pointless
         from .test_cli import invoke
 
-        (hosts, port) = swh_storage_cassandra_cluster
-        keyspace = "test" + os.urandom(10).hex()
-
-        storage_config = dict(
-            cls="cassandra",
-            hosts=hosts,
-            port=port,
-            keyspace=keyspace,
-            journal_writer={"cls": "memory"},
-            objstorage={"cls": "memory"},
-            auth_provider=cassandra_auth_provider_config,
+        result = invoke(
+            "cassandra",
+            "init",
+            local_config={"storage": swh_storage_cassandra_backend_config},
         )
-
-        result = invoke("cassandra", "init", local_config={"storage": storage_config})
         assert result.exit_code == 0, result.output
         assert result.output == "Done.\n"
 
         # Check we can write and read to it
-        storage = get_storage(**storage_config)
+        storage = get_storage(**swh_storage_cassandra_backend_config)
         origin = Origin(url="http://example.org")
         storage.origin_add([origin])
         assert storage.origin_get([origin.url]) == [origin]
