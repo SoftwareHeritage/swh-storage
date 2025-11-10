335a21b203e57e0ef6f6e847b3898d553c8866a2
Renaud Boyer <rboyer@softwareheritage.org>
2025-11-10 09:22:17 +0100

    algo: known SWHIDs

---
+++ swh/storage/algos/swhid.py
@@ -0,0 +1,84 @@
+# Copyright (C) 2025  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+from collections import defaultdict
+from typing import Iterable, TypeVar
+
+from swh.model.hashutil import hash_to_bytes
+from swh.model.swhids import (
+    CoreSWHID,
+    ExtendedObjectType,
+    ExtendedSWHID,
+    ObjectType,
+    QualifiedSWHID,
+)
+from swh.storage.interface import StorageInterface
+
+T = TypeVar("T", CoreSWHID, ExtendedSWHID, CoreSWHID | ExtendedSWHID)
+
+
+def known_swhids(storage: StorageInterface, swhids: Iterable[T]) -> set[T]:
+    """Query the storage to check if ``swhids`` exist.
+
+    We group SWHIDs by type and then call the corresponding storage ``_missing`` method
+    (directory_missing, snapshot_missing, etc.) and switch the results as we want to
+    know what exists, not what's missing.
+
+    As the storage does not use values from the qualifier (snapshot, etc.) we exclude
+    ``QualifiedSWHID`` from this method because
+    ``known_swhids(storage, swh:1:cnt:1234567890;visit=swh:1:snp:098654321)``
+    could return that `swh:1:cnt:1234567890` exists even if `swh:1:snp:098654321`
+    doesn't.
+
+    Args:
+        storage: a ``StorageInterface``
+        swhid: a list of SWHIDs
+
+    Raises:
+        TypeError: received a ``QualifiedSWHID`` in the ``swhids`` list
+
+    Returns:
+        A set of SWHIDs found in the storage
+    """
+
+    grouped_swhids: dict[ObjectType | ExtendedObjectType, list[T]] = defaultdict(list)
+    missing: set[bytes] = set()
+
+    for swhid in swhids:
+        if isinstance(swhid, QualifiedSWHID):
+            raise TypeError(
+                f"This method can't properly handle QualifiedSWHID like {swhid} "
+                "but only CoreSWHID or ExtendedSWHID"
+            )
+        grouped_swhids[swhid.object_type].append(swhid)
+
+    for object_type, objects in grouped_swhids.items():
+        if object_type == ObjectType.CONTENT:
+            storage_missing_method = storage.content_missing_per_sha1_git
+        else:
+            storage_missing_method = getattr(
+                storage, f"{object_type.name.lower()}_missing"
+            )
+
+        missing |= set(
+            storage_missing_method(
+                [hash_to_bytes(swhid.object_id) for swhid in objects]
+            )
+        )
+    return {swhid for swhid in swhids if hash_to_bytes(swhid.object_id) not in missing}
+
+
+def swhid_is_known(storage: StorageInterface, swhid: CoreSWHID | ExtendedSWHID) -> bool:
+    """Query the storage to check if ``swhid`` exists.
+
+    A wrapper for :meth:`known_swhids` but for a single SWHID.
+
+    Args:
+        storage: a ``StorageInterface``
+        swhid: a SWHID
+
+    Returns:
+        :const:`True` if ``swhid`` exists in the storage
+    """
+    return swhid in known_swhids(storage, [swhid])
---
+++ swh/storage/tests/algos/test_swhid.py
@@ -0,0 +1,85 @@
+# Copyright (C) 2025  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+import hashlib
+
+import pytest
+
+from swh.model.swhids import CoreSWHID, QualifiedSWHID
+from swh.storage.algos.swhid import known_swhids, swhid_is_known
+
+from ..storage_data import StorageData
+
+
+def test_known_swhids(swh_storage):
+    swh_storage.snapshot_add([StorageData.snapshot])
+    swh_storage.revision_add([StorageData.revision])
+    swh_storage.release_add([StorageData.release])
+    swh_storage.directory_add([StorageData.directory])
+    swh_storage.content_add([StorageData.content])
+
+    missing_snapshot_swhid = CoreSWHID.from_string(
+        f"swh:1:snp:{hashlib.sha1(b'test snapshot').hexdigest()}"
+    )
+    missing_revision_swhid = CoreSWHID.from_string(
+        f"swh:1:rev:{hashlib.sha1(b'test revision').hexdigest()}"
+    )
+    missing_release_swhid = CoreSWHID.from_string(
+        f"swh:1:rel:{hashlib.sha1(b'test release').hexdigest()}"
+    )
+    missing_directory_swhid = CoreSWHID.from_string(
+        f"swh:1:dir:{hashlib.sha1(b'test directory').hexdigest()}"
+    )
+    missing_content_swhid = CoreSWHID.from_string(
+        f"swh:1:cnt:{hashlib.sha1(b'test content').hexdigest()}"
+    )
+    results = known_swhids(
+        swh_storage,
+        [
+            StorageData.snapshot.swhid(),
+            StorageData.revision.swhid(),
+            StorageData.release.swhid(),
+            StorageData.directory.swhid(),
+            StorageData.content.swhid(),
+            missing_snapshot_swhid,
+            missing_revision_swhid,
+            missing_release_swhid,
+            missing_directory_swhid,
+            missing_content_swhid,
+        ],
+    )
+
+    assert len(results) == 5
+
+    assert StorageData.snapshot.swhid() in results
+    assert missing_snapshot_swhid not in results
+    assert StorageData.revision.swhid() in results
+    assert missing_revision_swhid not in results
+    assert StorageData.release.swhid() in results
+    assert missing_release_swhid not in results
+    assert StorageData.directory.swhid() in results
+    assert missing_directory_swhid not in results
+    assert StorageData.content.swhid() in results
+    assert missing_content_swhid not in results
+
+
+def test_swhid_is_known(swh_storage):
+    swh_storage.snapshot_add([StorageData.snapshot])
+    missing_snapshot_swhid = CoreSWHID.from_string(
+        f"swh:1:snp:{hashlib.sha1(b'test snapshot').hexdigest()}"
+    )
+    assert swhid_is_known(swh_storage, StorageData.snapshot.swhid())
+    assert not swhid_is_known(swh_storage, missing_snapshot_swhid)
+
+
+def test_does_not_handle_qualified_swhid(swh_storage):
+    swhid = f"swh:1:snp:{hashlib.sha1(b'test qualified').hexdigest()}"
+    qualified_swhid = QualifiedSWHID.from_string(swhid)
+    with pytest.raises(TypeError) as exc:
+        known_swhids(swh_storage, [qualified_swhid])
+    assert swhid in str(exc.value)
+
+    with pytest.raises(TypeError) as exc:
+        swhid_is_known(swh_storage, qualified_swhid)
+    assert swhid in str(exc.value)
