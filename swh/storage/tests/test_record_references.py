12376c494921f096f3f7fe64b6909ff4fbf12ad5
Antoine Lambert <anlambert@softwareheritage.org>
2026-01-23 17:05:37 +0100

    test_record_references: Add missing license header

--- swh/storage/tests/test_record_references.py
+++ swh/storage/tests/test_record_references.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2023-2024  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
 from collections import defaultdict
 import datetime
 from typing import Set, Tuple
